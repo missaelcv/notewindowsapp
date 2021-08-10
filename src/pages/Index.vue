0363abe5394f47169a46943fc1ed264590443de6
missaelcv <missaelcv0220@gmail.com>
2021-08-09 21:15:23 -0400

    Boton savework

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="q-pa-md q-gutter-sm">
-    <q-editor v-model="editor" min-height="5rem" />
+    <q-editor
+      v-model="editor"
+      :definitions="{
+        save: {
+          tip: 'Guardar task',
+          icon: 'save',
+          label: 'Guardar',
+          handler: saveWork
+        },
+        upload: {
+          tip: 'Actualizar Nube',
+          icon: 'cloud_upload',
+          label: 'Actualizar',
+          handler: uploadIt
+        }
+      }"
+      :toolbar="[
+        ['bold', 'italic', 'strike', 'underline'],
+        ['upload', 'save']
+      ]"/>
       
     <q-card flat bordered>
       <q-card-section v-html="editor" />
@@ -10,12 +29,31 @@
 
 <script>
 import { ref } from 'vue'
+import { useQuasar } from 'quasar'
 
 export default {
   setup () {
     return {
       editor: ''
     }
+  },
+  methods:{
+   saveWork () {
+        this.$q.notify({
+          message: 'Su Archivo ha sido Guardado en su Nube.',
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'cloud_done'
+        })
+      },
+      uploadIt () {
+       this.$q.notify({
+          message: 'Servidor Invalido Intente Conectar.',
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning'
+        })
+      }
   }
 }
 </script>
