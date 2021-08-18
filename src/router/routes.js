d99bc0020fe022bd0b5fddb5453b8a9c1b5f28ed
missaelcv <missaelcv0220@gmail.com>
2021-08-17 21:58:54 -0400

    nuevos router

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,10 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: '/nueva', component: () => import('pages/Nueva.vue') },
+      { path: '/ordenar', component: () => import('pages/Ordenar.vue') },
+
     ]
   },
 
