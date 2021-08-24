e11644c030bf6693bb020a968b207a033c4185d2
missaelcv <missaelcv0220@gmail.com>
2021-08-23 22:26:28 -0400

    nueva page agregarNota.vue

---
+++ src/pages/Agregar.vue
@@ -0,0 +1,19 @@
+<template>
+    <q-page padding>
+        
+        <h5 class="text-center">Agregar Nueva Nota</h5>
+        <q-btn label="Submit" color="secondary"/>
+
+         <q-btn label="Reset" color="brown-5" outline class="q-ml-sm"/>
+        </q-page>
+</template>
+
+
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
