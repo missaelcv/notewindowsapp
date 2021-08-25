837cdf12e650592a2bb3ffccd912fe01e69f54f9
missaelcv <missaelcv0220@gmail.com>
2021-08-24 23:20:05 -0400

    btn reset configurado

--- src/pages/Agregar.vue
+++ src/pages/Agregar.vue
@@ -2,23 +2,26 @@
     <q-page padding>
         
         <h5 class="text-center">Agregar Nueva Nota</h5>
-
-        {{nota}}-{{seleccion}}
-
+        {{nota}}-{{seleccion}}-{{terminos}}
         <q-form class="row q-col-gutter-md"
-        @submit.prevent = "procesarNota">
+        @submit.prevent = "procesarNota"
+        @reset="reset">
 
                 <div class="col-12 col-sm-6">
                  <q-input label="nota" v-model="nota" 
                  lazy-rules
-                 :rules="[ val => val && val.length > 0 || 'Please type something']"/>
+                 :rules="[ val => val && val.length > 0 || 'No Puede estar en blanco']"/>
                 </div>
 
                  <div class="col-12 col-sm-6">
                  <q-select label="Prioridad" v-model="seleccion" :options="opciones"
-                 :rules="[(val) => (val && val.length > 0) || 'Campo vacío']"/>
+                 :rules="[(val) => (val && val.length > 0) || 'No Puede estar en blanco']"/>
                 </div>
 
+                 <div class="col-12" >
+                    <q-toggle label = "Aceptar los Términos" 
+                    v-model="terminos"/>
+                </div>
 
             <div class="col-12">
         <q-btn label="Submit" color="secondary" type="submit"/>
@@ -30,23 +33,35 @@
 </template>
 
 <script>
-import {ref} from 'vue'
+import { useQuasar } from 'quasar'
+import { ref } from 'vue'
 
 export default {
     setup() {
         const nota = ref(null)
         const seleccion = ref(null)
+        const terminos = ref(false)
         const opciones = ['Maxima', 'Regular','Minima']
 
         const procesarNota = () => {
             console.log('clic de nota en consola')
+            if(terminos.value === false)
+            alert('Falto los términos')
+        }
+        const reset = () => {
+            nota.value = null
+            seleccion.value = null
+            terminos.value = false 
+
         }
 
         return {
             nota,
             seleccion,
             opciones,
-            procesarNota
+            procesarNota,
+            terminos,
+            reset
         }
     },
 }
