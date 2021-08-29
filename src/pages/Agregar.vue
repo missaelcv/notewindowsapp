dc9ad11d1e67d5d753a4a345489529ce0df2d428
missaelcv <missaelcv0220@gmail.com>
2021-08-29 15:48:36 -0400

    monstrar sin notas

--- src/pages/Agregar.vue
+++ src/pages/Agregar.vue
@@ -10,23 +10,57 @@
       
                 <div class="col-12 col-sm-6">
 
-                 <q-input label="Nota" v-model="Nota" 
+                 <q-input label="Nombre de la nota" v-model="Nota" 
                  lazy-rules
                  :rules="[ val => val && val.length > 0 || 'No Puede estar en blanco']"/>
                 </div>
 
                  <div class="col-12 col-sm-6">
 
-                 <q-select label="Prioridad" v-model="seleccion" 
+                 <q-select label="Prioridad de su nota" v-model="seleccion" 
                  :options="opciones"
                  lazy-rules
                  :rules="[ val => val && val.length > 0 || 'No Puede estar en blanco']"/>
                 </div>
 
+              
+    <div class="col-12 col-sm-12">
+    <q-editor v-model="editor"
+      :definitions="{
+        save: {
+          tip: 'Guardar task',
+          icon: 'save',
+          label: 'Guardar',
+          handler: saveWork
+        },
+        upload: {
+          tip: 'Actualizar Nube',
+          icon: 'cloud_upload',
+          label: 'Actualizar',
+          handler: uploadIt
+        }
+      }"
+      :toolbar="[
+        ['bold', 'italic', 'strike', 'underline'],
+        ['upload', 'save']]"/>
+               </div>
+
                  <div class="col-12" >
                     <q-toggle label = "Aceptar los Términos" 
                     v-model="terminos"/>
                 </div>
+
+            <q-card class="row"
+            flat bordered v-for="(item,index) in tasks" :key="index" >
+                <q-card-section class="col" v-html="item.texto" 
+                :class="item.estado ? 'tachar' : ''" />
+                <q-btn flat color="blue" @click="item.estado = !item.estado">Acción</q-btn>
+                <q-btn flat color="red" @click="eliminar(index)">Eliminar</q-btn>
+
+             </q-card>
+            <div v-if="tasks.length == 0" class="flex flex-center">
+            <p class="text-h6">Sin Notas</p>
+            </div>
 
             <div class="col-12">
         <q-btn label="Submit" color="secondary" type="submit"/>
@@ -34,6 +68,9 @@
          <q-btn label="Reset" color="brown-5" outline class="q-ml-sm" type="reset"/>
          </div>
          </q-form>
+
+        
+         
         </q-page>
 </template>
 
@@ -65,7 +102,8 @@ export default {
                     textColor: 'white',
                     icon: 'cloud_done',
                     message: 'Nota Guardad'
-            })
+            }),
+            
              myForm.value.resetValidation();
             reset()
             }
@@ -77,15 +115,56 @@ export default {
             terminos.value = false 
         }
         return {
+            
             Nota,
             seleccion,
             opciones,
             procesarNota,
             terminos,
             reset,
-            myForm
-            
-        }
+            myForm,
+
+         editor: '',
+      tasks: [
+      //    {texto: 'Nota #1', estado: false},
+      //    {texto: 'Nota #2', estado: false},
+      //    {texto: 'Nota #3', estado: false},
+      ]
     }
+  },
+  methods:{
+   saveWork () {
+     this.tasks.push({
+       texto: this.editor,
+       estado: false
+     })
+        this.$q.notify({
+          message: 'Nota Guardada',
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'cloud_done'
+        })
+      },
+      uploadIt () {
+       this.$q.notify({
+          message: 'Servidor Invalido Intente Conectar.',
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning'
+        })
+      },
+  eliminar(index){
+   this.$q.dialog({
+    title: 'Adventencia',
+    message: 'Esta seguro de eliminar su nota',
+    cancel: true,
+    persistent: true
+  }).onOk(() => {
+    // console.log('>>>> OK')
+     this.tasks.splice(index, 1);
+  })
 }
+}
+}
+
 </script>
