b0f15fac16ed82791b1576b3b91786658dc8f428
missaelcv <missaelcv0220@gmail.com>
2021-10-16 22:52:25 -0400

    Account Settings

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,50 +1,21 @@
 <template>
-  <div class="q-pa-md">
-    <q-stepper
-      v-model="step"
-      ref="stepper"
-      alternative-labels
-      color="primary"
-      animated
+  <div class="q-pa-md" style="max-width: 350px">
+    <q-toggle v-model="expanded" label="Expanded" class="q-mb-md" />
+
+    <q-expansion-item
+      v-model="expanded"
+      icon="perm_identity"
+      label="Account settings"
+      caption="Usuario"
     >
-      <q-step
-        :name="1"
-        title="Select campaign settings"
-        icon="settings"
-        :done="step > 1"
-      >
-        For each ad campaign that you create, you can control how much you're willing to
-        spend on clicks and conversions, which networks and geographical locations you want
-        your ads to show on, and more.
-      </q-step>
-
-      <q-step
-        :name="2"
-        title="Create an ad group"
-        caption="Optional"
-        icon="create_new_folder"
-        :done="step > 2"
-      >
-        An ad group contains one or more ads which target a shared set of keywords.
-      </q-step>
-
-      <q-step
-        :name="3"
-        title="Create an ad"
-        icon="add_comment"
-      >
-        Try out different ad text to see what brings in the most customers, and learn how to
-        enhance your ads using features like ad extensions. If you run into any problems with
-        your ads, find out how to tell if they're running and how to resolve approval issues.
-      </q-step>
-
-      <template v-slot:navigation>
-        <q-stepper-navigation>
-          <q-btn @click="$refs.stepper.next()" color="primary" :label="step === 3 ? 'Finish' : 'Continue'" />
-          <q-btn v-if="step > 1" flat color="primary" @click="$refs.stepper.previous()" label="Back" class="q-ml-sm" />
-        </q-stepper-navigation>
-      </template>
-    </q-stepper>
+      <q-card>
+        <q-card-section>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+          commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+          eveniet doloribus ullam aliquid.
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
   </div>
 </template>
 
@@ -54,7 +25,7 @@ import { ref } from 'vue'
 export default {
   setup () {
     return {
-      step: ref(1)
+      expanded: ref(false)
     }
   }
 }
