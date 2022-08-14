2945b04c63bfdd1a75f76b211efc15089a7f12ae
missaelcv <missaelcv0220@gmail.com>
2022-08-13 23:49:10 -0400

    firebase

---
+++ src/Utils/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyA_zgHY1mO3-GFjnjgAAnkmrmuyE8TVt1I",
+    authDomain: "fir-vue-tes.firebaseapp.com",
+    projectId: "fir-vue-tes",
+    storageBucket: "fir-vue-tes.appspot.com",
+    messagingSenderId: "411222402216",
+    appId: "1:411222402216:web:11bd24bda1f36404560e9d"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  
+
